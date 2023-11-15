9239443831d8be2872e5570020c7b83ad64fb7c3
rboudrouss <r.boudrouss@gmail.com>
2023-11-15 23:53:49 +0100

    added compression of imgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://playsorbonne.fr/',
-
-  integrations: [react()]
+  integrations: [react(), compress()]
 });
