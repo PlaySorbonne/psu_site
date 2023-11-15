6eebeaeafe5c904a77d4f0b855d09f29f65f8a2e
rboudrouss <r.boudrouss@gmail.com>
2023-11-15 23:35:05 +0100

    added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://playsorbonne.fr/',
+
+  integrations: [react()]
 });
