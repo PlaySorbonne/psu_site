c6781a53bfaf7d597a13b0a2a60828e7958a21f8
rboudrouss <r.boudrouss@gmail.com>
2024-01-01 20:58:08 +0100

    renamed slides to events

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,51 @@
 import type { MarkdownInstance, MDXInstance } from "astro";
 
+/*
+ * Sort slides by priority and remove duplicates
+ * @param slides Array of slides
+ * @returns Sorted array of slides
+ */
+export function sortSlides(slides: EventT[]): EventT[] {
+  let priority: EventT[] = [];
+  let other: EventT[] = [];
+  slides.forEach((slide) => {
+    // if (slide.inCarrousel === false) return;
+    if (slide.priority) priority.push(slide);
+    else other.push(slide);
+  });
+  return priority.concat(other).reduce((acc, cur) => {
+    if (acc.find((e) => e.src === cur.src)) return acc;
+    return acc.concat(cur);
+  }, [] as EventT[]);
+}
+
+export function filterNonCarrousel(slides: EventT[]): EventT[] {
+  return slides.filter((e) => e.inCarrousel !== false);
+}
+
+export function rawMDtoSlide(e: MDnXInstance<EventT>): EventT {
+  return {
+    title: e.frontmatter.title,
+    subtitle: e.frontmatter.subtitle,
+    src: e.frontmatter.src,
+    alt: e.frontmatter.alt ?? "",
+    priority: e.frontmatter.priority ?? false,
+    link: e.url,
+    noLink: e.frontmatter.noLink ?? false,
+    description: e.frontmatter.description,
+    icon: e.frontmatter.icon ?? "",
+  };
+}
+
+export function rawMDtoSortedArray(e: MDnXInstance<EventT>[]): EventT[] {
+  return sortSlides(e.map(rawMDtoSlide));
+}
+
 export type ClubName = "dlc" | "luxludi" | "psu" | "pls" | "champsu";
 
 export type MDnXInstance<T> = MarkdownInstance<T> | MDXInstance<T>;
 
-export interface Slide {
+export interface EventT {
   title: string;
   subtitle?: string;
   src: string;
@@ -13,6 +54,8 @@ export interface Slide {
   noLink?: boolean;
   priority?: boolean;
   inCarrousel?: boolean;
+  description?: string;
+  icon?: string;
 }
 
 interface Page {
@@ -51,38 +94,3 @@ export const pages: Page[] = [
     ],
   }, */
 ];
-
-/*
- * Sort slides by priority and remove duplicates
- * @param slides Array of slides
- * @returns Sorted array of slides
- */
-export function sortSlides(slides: Slide[]): Slide[] {
-  let priority: Slide[] = [];
-  let other: Slide[] = [];
-  slides.forEach((slide) => {
-    if (slide.inCarrousel === false) return;
-    if (slide.priority) priority.push(slide);
-    else other.push(slide);
-  });
-  return priority.concat(other).reduce((acc, cur) => {
-    if (acc.find((e) => e.src === cur.src)) return acc;
-    return acc.concat(cur);
-  }, [] as Slide[]);
-}
-
-export function rawMDtoSlide(e: MDnXInstance<Slide>): Slide {
-  return {
-    title: e.frontmatter.title,
-    subtitle: e.frontmatter.subtitle,
-    src: e.frontmatter.src,
-    alt: e.frontmatter.alt ?? "",
-    priority: e.frontmatter.priority ?? false,
-    link: e.url,
-    noLink: e.frontmatter.noLink ?? false,
-  };
-}
-
-export function rawMDtoSortedArray(e: MDnXInstance<Slide>[]): Slide[] {
-  return sortSlides(e.map(rawMDtoSlide));
-}
