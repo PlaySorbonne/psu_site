6b2c00e0265a18c46563d662a509941583dafaca
rboudrouss <r.boudrouss@gmail.com>
2024-01-23 02:13:17 +0100

    added champsuplot

--- src/assets/events/index.ts
+++ src/assets/events/index.ts
@@ -2,11 +2,13 @@ import type { ImageMetadata } from "astro";
 import unknown from "./base.png";
 import conf from "./conf.png";
 import smash from "./smash.png";
+import travaux from "./travaux.webp";
 
 export const eventsIcons = {
   unknown,
   conf,
   smash,
+  travaux
 } as const;
 
 export function getEventIcon(name: string): ImageMetadata{
