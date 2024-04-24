3476b7a1f4b4e0259ff8a77f351c4e1bcab0a3e1
rboudrouss <r.boudrouss@gmail.com>
2024-04-24 21:53:21 +0200

    popup will not go

--- src/helpers/popups.ts
+++ src/helpers/popups.ts
@@ -1,17 +1,19 @@
 const devMode: boolean = !!import.meta.env.PUBLIC_DEV_MODE;
 
 const out = [
+  devMode && {
+    content:
+      "Ce site est une demo ! Le site officiel est sur [playsorbonne.fr](https://playsorbonne.fr)",
+    type: "floating",
+    position: "top",
+    closeable: false,
+  },
   {
     content:
       "Les inscriptions pour [**la formation civique et citoyenne**](/formation) sont ouvertes !",
     type: "floating",
     position: "top",
   },
-  devMode && {
-    content: "Ce site est une demo ! Le site officiel est sur [playsorbonne.fr](https://playsorbonne.fr)",
-    type: "floating",
-    position: "top"
-  }
 ];
 
-export default out.filter(e => e);
+export default out.filter((e) => e);
