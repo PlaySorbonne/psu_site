8a1440d958b9b9904d8381fcbda4c5124661fe00
rboudrouss <r.boudrouss@gmail.com>
2024-08-16 03:16:48 +0200

    infos

---
+++ src/assets/festival/j2s/index.ts
@@ -0,0 +1,13 @@
+import eternal_quest from './eternal_quest.jpg';
+import eternal_quest2 from './eternal_quest2.png';
+import kagoun from './kagoun.jpeg';
+import kagoun2 from './kagoun2.jpeg';
+import hunting_grounds from './hunting_grounds.jpg';
+
+export default {
+  eternal_quest,
+  eternal_quest2,
+  kagoun,
+  kagoun2,
+  hunting_grounds,
+};
---
+++ src/assets/festival/jv/index.ts
@@ -0,0 +1,31 @@
+import demon_girls from "./demon_girls.png";
+import demon_girls2 from "./demon_girls2.png";
+import doxu from "./doxu.png";
+import doxu2 from "./doxu2.png";
+import endseeker from "./endseeker.png";
+import endseeker2 from "./endseeker2.png";
+import froggys_battle from "./froggys_battle.png";
+import froggys_battle2 from "./froggys_battle2.png";
+import lava_loop from "./lava_loop.png";
+import lava_loop2 from "./lava_loop2.png";
+import minami_lane from "./minami_lane.png";
+import minami_lane2 from "./minami_lane2.png";
+import seccia from "./seccia.png";
+import seccia2 from "./seccia2.png";
+
+export default {
+  demon_girls,
+  demon_girls2,
+  doxu,
+  doxu2,
+  endseeker,
+  endseeker2,
+  froggys_battle,
+  froggys_battle2,
+  lava_loop,
+  lava_loop2,
+  minami_lane,
+  minami_lane2,
+  seccia,
+  seccia2,
+};
