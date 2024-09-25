d8f77218f4aabcd270c31407e826be95e1ce9710
rboudrouss <r.boudrouss@gmail.com>
2024-09-25 19:05:20 +0200

    update dev indé

--- src/assets/festival/j2s/index.ts
+++ src/assets/festival/j2s/index.ts
@@ -6,9 +6,29 @@ import hunting_grounds from './hunting_grounds.jpg';
 import memocombo from './memocombo.png';
 import tribunazes from './tribunazes.png';
 import cheval from './cheval.png';
-import leaf from './leaf.jpg';
-import leaf2 from './leaf2.jpg';
 import legendofgonia from './legendofgonia.png';
+import soukabroca from "./soukabroca.jpg";
+import pfsc from "./pfsc.png";
+import masques from "./masques.png";
+import olem from "./olem.png";
+import mycelium from "./mycelium.png";
+import maktila from "./maktila.jpg";
+import sahara from "./sahara.jpg";
+import voyant from "./voyant.webp";
+import ludimino from "./ludimino.png";
+import course from "./course.png";
+import archipel from "./archipel.png";
+import ligue from "./ligue.png";
+import memes from "./memes.png";
+import donjons from "./donjons.jpg";
+import diceclash from "./diceclash.jpg";
+import ddg from "./ddg.png";
+import cas from "./cas.png";
+import coverta from "./coverta.png";
+import brutus from "./brutus.jpg";
+import alderous from "./alderous.jpg";
+import pub from "./pub.jpg";
+
 
 export default {
   eternal_quest,
@@ -19,7 +39,26 @@ export default {
   memocombo,
   tribunazes,
   cheval,
-  leaf,
-  leaf2,
   legendofgonia,
+  soukabroca,
+  pfsc,
+  masques,
+  olem,
+  mycelium,
+  maktila,
+  sahara,
+  voyant,
+  ludimino,
+  course,
+  archipel,
+  ligue,
+  memes,
+  donjons,
+  diceclash,
+  ddg,
+  cas,
+  coverta,
+  brutus,
+  alderous,
+  pub
 }
